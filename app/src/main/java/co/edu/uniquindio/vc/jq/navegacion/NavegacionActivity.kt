306665693ed684dda5d06907a99873d158cff9a8
vacacoi <vicalf.11@hotmail.com>
2018-08-17 18:59:20 -0500

    Se crea actividad de ventana dos

--- app/src/main/java/co/edu/uniquindio/vc/jq/navegacion/NavegacionActivity.kt
+++ app/src/main/java/co/edu/uniquindio/vc/jq/navegacion/NavegacionActivity.kt
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.vc.jq.navegacion
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,9 +19,15 @@ class NavegacionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_navegacion)
     }
 
+    /**
+     * Permite pasar a ventana dos
+     * @param view botón que genera el evento
+     */
     fun pasarAVentanados(view: View){
 
-        Log.v("NavegacionActivity","Voy a pasar a ventana dos prueba")
+       // Log.v("NavegacionActivity","Voy a pasar a ventana dos prueba")
+        val intent = Intent(this,VentanaDosActivity::class.java);
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/co/edu/uniquindio/vc/jq/navegacion/VentanaDosActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.vc.jq.navegacion
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VentanaDosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana_dos)
+    }
+}
