f1bad730898414bb35b5f06f1022d790e9ecdecc
vacacoi <vicalf.11@hotmail.com>
2018-08-16 23:07:11 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/uniquindio/vc/jq/navegacion/NavegacionActivity.kt
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.vc.jq.navegacion
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+
+/**
+ * Actividad encargada de manejar navegación de la App
+ * @author JQ--VC
+ * @version 1.0
+ */
+class NavegacionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navegacion)
+    }
+
+    fun pasarAVentanados(view: View){
+
+        Log.v("NavegacionActivity","Voy a pasar a ventana dos")
+
+    }
+}
